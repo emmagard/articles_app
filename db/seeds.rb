974d2804c74d486ea67252e3be98be1a83818240
Emma Gardner <emmagardner@outlook.com>
2014-07-22 15:46:47 -0400

    added seed data for articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+Article.delete_all
+
+12.times do |i|
+  art = Article.create!(title: "Article #{i}", body: "Body #{i}")
+  puts "Created Article #{art.inspect}"
+end
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
